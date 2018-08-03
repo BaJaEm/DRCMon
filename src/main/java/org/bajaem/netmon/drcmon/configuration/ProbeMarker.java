306b57a109e1fc24a097532c323fafc5e49452f4
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-03 18:02:46 -0400

    Fix Annotation to be visible at runtime

--- src/main/java/org/bajaem/netmon/drcmon/configuration/ProbeMarker.java
+++ src/main/java/org/bajaem/netmon/drcmon/configuration/ProbeMarker.java
@@ -1,5 +1,12 @@
 package org.bajaem.netmon.drcmon.configuration;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value=ElementType.TYPE)
 public @interface ProbeMarker
 {
     String name();
