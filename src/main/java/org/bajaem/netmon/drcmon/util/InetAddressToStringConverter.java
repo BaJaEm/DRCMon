e79a5a8e4f4eada4715ff4561cfa0fa12b998c65
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-03 10:41:49 -0400

    added converter for Host types ( INetAddress )

---
+++ src/main/java/org/bajaem/netmon/drcmon/util/InetAddressToStringConverter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.bajaem.netmon.drcmon.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.persistence.AttributeConverter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+
+ *
+ */
+public class InetAddressToStringConverter implements AttributeConverter<InetAddress, String>
+{
+    private static final Logger LOG = LogManager.getLogger(InetAddressToStringConverter.class);
+
+    @Override
+    public String convertToDatabaseColumn(final InetAddress value)
+    {
+        return null != value ? value.getCanonicalHostName() : null;
+    }
+
+    @Override
+    public InetAddress convertToEntityAttribute(final String value)
+    {
+        try
+        {
+
+            return InetAddress.getByName(value);
+        }
+        catch (final UnknownHostException e)
+        {
+            LOG.fatal(e);
+            throw new RuntimeException(e);
+        }
+    }
+}
