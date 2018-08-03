2e4e0b5c9940cb2fdbd2c13834c096bfaf8e9049
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-03 18:08:32 -0400

    Clean up formatting

--- src/main/java/org/bajaem/netmon/drcmon/probe/PingProbe.java
+++ src/main/java/org/bajaem/netmon/drcmon/probe/PingProbe.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.bajaem.netmon.drcmon.configuration.ProbeMarker;
 import org.bajaem.netmon.drcmon.model.ProbeConfig;
 
-@ProbeMarker(name="Ping")
+@ProbeMarker(name = "Ping")
 public class PingProbe extends Probe
 {
     private static final Logger LOG = LogManager.getLogger(PingProbe.class);
