1d472596d2bba62a26572f069a14fdf1d2561aad
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-05 13:16:25 -0400

    make name unique for a probe_type

--- src/main/resources/sql/probe_type.sql
+++ src/main/resources/sql/probe_type.sql
@@ -4,8 +4,9 @@ CREATE TABLE probe_type
 	name			VARCHAR(50) NOT NULL,
 	description     VARCHAR(255) NOT NULL
 );
--- TODO make name unique
+
+ALTER TABLE probe_type ADD CONSTRAINT  probe_type_uq_name UNIQUE (name);
+
+
 INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'Ping', 'Ping using java isReachable method - may or maynot be ICMP');
-
 INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'PortMon', 'TCP Port Monitor - Checks if port is reachable');
-
