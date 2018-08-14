d47ea05334474c42e6af5131d1c966b488de40ac
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-14 17:31:59 -0400

    Add permissions to Repositories
    For Updates the logged in user must be an admin

--- src/main/java/org/bajaem/netmon/drcmon/respository/ProbeTypeRepository.java
+++ src/main/java/org/bajaem/netmon/drcmon/respository/ProbeTypeRepository.java
@@ -1,11 +1,23 @@
+
 package org.bajaem.netmon.drcmon.respository;
 
 import org.bajaem.netmon.drcmon.model.ProbeType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasRole('ROLE_USER')")
 @RepositoryRestResource
 public interface ProbeTypeRepository extends CrudRepository<ProbeType, Long>
 {
+
     public ProbeType findByName(final String name);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    <S extends ProbeType> S save(S _entity);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(ProbeType aLong);
 }
