eb9da7babaa7f16d0ebeaf8bfeb24b43745a694a
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-16 13:02:28 -0400

    more filling in Web Probe
    Use JsonTools to get value - use username passwords from key files

--- src/main/java/org/bajaem/drcmon/probe/RESTGetProbe.java
+++ src/main/java/org/bajaem/drcmon/probe/RESTGetProbe.java
@@ -4,9 +4,14 @@ package org.bajaem.drcmon.probe;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bajaem.drcmon.configuration.ProbeMarker;
+import org.bajaem.drcmon.exceptions.DRCProbeException;
 import org.bajaem.drcmon.model.ProbeConfig;
-import org.bajaem.drcmon.util.DRCRestTemplate;
+import org.bajaem.drcmon.util.DRCBasicAuthRestWeb;
 import org.bajaem.drcmon.util.DRCWebClient;
+import org.bajaem.drcmon.util.JsonTools;
+import org.bajaem.drcmon.util.Key;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 @ProbeMarker(name = "RESTGetProbe")
 public class RESTGetProbe extends Probe
@@ -16,25 +21,40 @@ public class RESTGetProbe extends Probe
 
     private final String url;
 
-    private final String response;
+    private final String expected;
+
+    private final String path;
 
     private final DRCWebClient client;
+
+    private final Key key;
 
     public RESTGetProbe(final ProbeConfig _probeConfig)
     {
         super(_probeConfig);
         LOG.trace("New Probe... " + getProbeConfig().getHost());
         url = getProbeConfig().getCustomConfiguration().get("url");
-        response = getProbeConfig().getCustomConfiguration().get("response");
+        expected = getProbeConfig().getCustomConfiguration().get("expected");
+        path = getProbeConfig().getCustomConfiguration().get("path");
+        key = Key.decryptKey(getProbeConfig().getCustomConfiguration().get("keyFile"));
         // TODO: use factory
-        client = new DRCRestTemplate(url);
+        client = new DRCBasicAuthRestWeb(url, key.getId(), key.getSecret());
 
     }
 
     @Override
     public Response probe()
     {
-        return new Response(response.equals(client.get(String.class)));
+        final JsonNode node = client.get();
+        try
+        {
+            return new Response(expected.equals(JsonTools.getValue(node, path)));
+        }
+        catch (final DRCProbeException e)
+        {
+            return new Response(false, e.getMessage(), e);
+        }
+
     }
 
     @Override
