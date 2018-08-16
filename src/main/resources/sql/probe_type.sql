b631fed292228110b3bb029f5a45280764a28369
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-16 17:16:40 -0400

    Add RestWeb to Probe types

--- src/main/resources/sql/probe_type.sql
+++ src/main/resources/sql/probe_type.sql
@@ -11,3 +11,4 @@ ALTER TABLE probe_type ADD CONSTRAINT  probe_type_uq_name UNIQUE (name);
 INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'Ping', 'Ping using java isReachable method - may or maynot be ICMP');
 INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'PortMon', 'TCP Port Monitor - Checks if port is reachable');
 INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'SQLQueryProbe', 'SQL Query Probe - checks if a query returns correct value');
+INSERT INTO probe_type (id, name, description) VALUES (key_seq.nextval, 'RESTGetProbe', 'Web Query Probe - checks if a REST GET returns correct value');
