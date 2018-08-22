b390c96162925ffb485ca9bc910fb9d80babb034
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-22 12:04:59 -0400

    Adding REST Controller to expose start/stop/status via REST calls

---
+++ src/main/java/org/bajaem/drcmon/controller/DRCRestController.java
@@ -0,0 +1,43 @@
+
+package org.bajaem.drcmon.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bajaem.drcmon.engine.MonitorEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Secured("ROLE_ADMIN")
+public class DRCRestController
+{
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    @Autowired
+    private MonitorEngine engine;
+
+    @RequestMapping("/start")
+    public void start()
+    {
+        LOG.info("Starting");
+        engine.start();
+    }
+
+    @RequestMapping("/stop")
+    public void stop()
+    {
+        LOG.info("Stopping...");
+        engine.stop();
+    }
+
+    @Secured("ROLE_USER")
+    @RequestMapping("/status")
+    public boolean status()
+    {
+        return engine.isRunning();
+    }
+
+}
