e514609f92dcbb24632a3fe0b2a5f06d167229a0
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-25 02:42:39 -0400

    Added enabled on the probe_config table

--- src/main/resources/sql/probe_config.sql
+++ src/main/resources/sql/probe_config.sql
@@ -7,10 +7,11 @@ CREATE OR REPLACE TABLE probe_config
 	probe_type       	  VARCHAR(10) NOT NULL,
 	polling_interval	  INT not null,
 	delay_time			  INT,
-	created_on			  datetime not null,
+	created_on			  datetime not null DEFAULT CURRENT_TIMESTAMP,
 	created_by			  varchar(50) not null,
-	last_modified_on      datetime not null,
+	last_modified_on      datetime not null  DEFAULT CURRENT_TIMESTAMP,
 	last_modified_by      varchar(50) not null,
+	enabled               CHAR(1) DEFAULT 'T' NOT NULL CHECK (enabled IN ('T', 'F')),
 	custom_configuration  varchar(2048)
 	
 	
@@ -19,3 +20,4 @@ CREATE OR REPLACE TABLE probe_config
 ALTER TABLE probe_config 
 	ADD FOREIGN KEY ( probe_type )
 	REFERENCES probe_type ( name );
+	
