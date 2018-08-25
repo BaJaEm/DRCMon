6cbe94ff11b2faaa2bd3f66d686d328a6c26ba53
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-25 02:43:35 -0400

    Starting to replace some of the SPRING DATA REST with direct calls
    With the hierarchy the is being developed, we need better access to the
    internals and can't just process the requests as a "black box".

--- src/main/java/org/bajaem/drcmon/controller/DRCRestController.java
+++ src/main/java/org/bajaem/drcmon/controller/DRCRestController.java
@@ -4,9 +4,13 @@ package org.bajaem.drcmon.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bajaem.drcmon.engine.MonitorEngine;
+import org.bajaem.drcmon.model.ProbeConfig;
+import org.bajaem.drcmon.respository.ProbeConfigRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +19,9 @@ public class DRCRestController
 {
 
     private static final Logger LOG = LogManager.getLogger();
+
+    @Autowired
+    ProbeConfigRepository repo;
 
     @Autowired
     private MonitorEngine engine;
@@ -38,6 +45,32 @@ public class DRCRestController
     public boolean status()
     {
         return engine.isRunning();
+    }
+
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(path = "/repo/probeConfig/{id}", method = RequestMethod.GET)
+    public ProbeConfig get(@PathVariable("id") final long id)
+    {
+        final ProbeConfig config = repo.findById(id).get();
+        return config;
+    }
+
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(path = "/repo/probeConfig/{id}", method = RequestMethod.PUT)
+    public ProbeConfig save(final String input, @PathVariable("id") final long id)
+    {
+        LOG.info("Got Here: " + id + " \n\n\n\n\n\n\n\n\n" + input);
+        final ProbeConfig config = repo.findById(id).get();
+        LOG.info("Config: " + config);
+        return config;
+    }
+
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(path = "/repo/probeConfigs", method = RequestMethod.GET)
+    public Iterable<ProbeConfig> getAll()
+    {
+        final Iterable<ProbeConfig> config = repo.findAll();
+        return config;
     }
 
 }
