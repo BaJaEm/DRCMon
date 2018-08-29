bac1963350a98a7b3b966412e5f2c1ffe445fcca
BaJaEm DRC <bajaemdrc@gmail.com>
2018-08-29 18:17:04 -0400

    MovedProjection into sub-package of model package

---
+++ src/main/java/org/bajaem/drcmon/model/projection/ProbeConfigProjection.java
@@ -0,0 +1,34 @@
+
+package org.bajaem.drcmon.model.projection;
+
+import java.sql.Timestamp;
+import java.util.Map;
+
+import org.bajaem.drcmon.model.ProbeConfig;
+import org.bajaem.drcmon.model.ProbeType;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "full", types = { ProbeConfig.class })
+public interface ProbeConfigProjection
+{
+
+    public String getArtifactId();
+
+    public int getPollingInterval();
+
+    public int getDelayTime();
+
+    public Timestamp getCreatedOn();
+
+    public String getCreatedBy();
+
+    public Timestamp getLastModifiedOn();
+
+    public String getLastModifiedBy();
+
+    public boolean isEnabled();
+
+    public ProbeType getProbeType();
+
+    public Map<String, String> getCustomConfiguration();
+}
--- src/main/java/org/bajaem/drcmon/respository/ProbeConfigRepository.java
+++ src/main/java/org/bajaem/drcmon/respository/ProbeConfigRepository.java
@@ -2,12 +2,13 @@
 package org.bajaem.drcmon.respository;
 
 import org.bajaem.drcmon.model.ProbeConfig;
+import org.bajaem.drcmon.model.projection.ProbeConfigProjection;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @PreAuthorize("hasRole('ROLE_USER')")
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = ProbeConfigProjection.class)
 public interface ProbeConfigRepository extends CrudRepository<ProbeConfig, Long>
 {
 
