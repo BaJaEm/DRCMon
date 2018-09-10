aee504c417b42a0571bdef46dffd9a6643349968
BaJaEm DRC <bajaemdrc@gmail.com>
2018-09-10 13:57:50 -0400

    Go Back to using Calendar rather than Instance

--- src/main/java/org/bajaem/drcmon/util/SystemClock.java
+++ src/main/java/org/bajaem/drcmon/util/SystemClock.java
@@ -1,13 +1,13 @@
 package org.bajaem.drcmon.util;
 
 import java.sql.Timestamp;
-import java.time.Instant;
+import java.util.Calendar;
 
 public class SystemClock
 {
 
     public static Timestamp currentTime()
     {
-        return new Timestamp(Instant.now().toEpochMilli());
+        return new Timestamp(Calendar.getInstance().getTimeInMillis());
     }
 }
