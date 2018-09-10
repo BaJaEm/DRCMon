cb8f22c0cb94f52f4505c3293673eea0cd44c0c6
BaJaEm DRC <bajaemdrc@gmail.com>
2018-09-10 14:14:28 -0400

    Add method to find Responses by ProbeConfig

--- src/main/java/org/bajaem/drcmon/respository/ProbeResponseRepository.java
+++ src/main/java/org/bajaem/drcmon/respository/ProbeResponseRepository.java
@@ -2,14 +2,21 @@
 package org.bajaem.drcmon.respository;
 
 import org.bajaem.drcmon.model.ProbeResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @PreAuthorize("hasRole('ROLE_USER')")
 @RepositoryRestResource
 public interface ProbeResponseRepository extends PagingAndSortingRepository<ProbeResponse, Long>
 {
+
+    @RestResource
+    public Page<ProbeResponse> findByProbeConfig_Id(@Param(value = "id") final Long id, final Pageable p);
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Override
