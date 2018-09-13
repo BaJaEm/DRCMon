4f9ebe2ff04f148ab78cfa3f8ac1d9543b67474e
BaJaEm DRC <bajaemdrc@gmail.com>
2018-09-13 14:01:11 -0400

    Fix Up Message Sender Service

--- src/main/java/org/bajaem/drcmon/mq/MessageSender.java
+++ src/main/java/org/bajaem/drcmon/mq/MessageSender.java
@@ -1,45 +1,26 @@
 package org.bajaem.drcmon.mq;
 
-import javax.jms.ConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bajaem.drcmon.model.ProbeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-import org.springframework.stereotype.Service;
-
-@Service
+@EnableJms
+@Component
 public class MessageSender
 {
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LogManager.getLogger();
 
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(final ConnectionFactory connectionFactory,
-            final DefaultJmsListenerContainerFactoryConfigurer configurer)
-    {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        configurer.configure(factory, connectionFactory);
-        factory.setClientId("my-client");
-        factory.setSubscriptionDurable(true);
-        factory.setConcurrency("3-10");
-        return factory;
-    }
+    @Autowired
+    JmsTemplate jmsTemplate;
 
-    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter()
+    public void sendMessage(final ProbeResponse resp)
     {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
-
-    @JmsListener(destination = "drcmon.topic", subscription="drcmon.sub", containerFactory = "myFactory")
-    public void receiveMessage(String msg)
-    {
-        System.out.println("Received <" + msg + ">");
+        jmsTemplate.setPubSubDomain(true);
+        jmsTemplate.convertAndSend("drcmon.topic", resp);
     }
 }
