7f8769cb97e0981baae47030326b9326df432085
BaJaEm DRC <bajaemdrc@gmail.com>
2018-09-13 16:53:03 -0400

    Remove un-used methods from RESTController

--- src/main/java/org/bajaem/drcmon/controller/DRCRestController.java
+++ src/main/java/org/bajaem/drcmon/controller/DRCRestController.java
@@ -4,13 +4,10 @@ package org.bajaem.drcmon.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bajaem.drcmon.engine.MonitorEngine;
-import org.bajaem.drcmon.model.ProbeConfig;
 import org.bajaem.drcmon.respository.ProbeConfigRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -46,31 +43,4 @@ public class DRCRestController
     {
         return engine.isRunning();
     }
-
-    @Secured("ROLE_ADMIN")
-    @RequestMapping(path = "/repo/probeConfig/{id}", method = RequestMethod.GET)
-    public ProbeConfig get(@PathVariable("id") final long id)
-    {
-        final ProbeConfig config = repo.findById(id).get();
-        return config;
-    }
-
-    @Secured("ROLE_ADMIN")
-    @RequestMapping(path = "/repo/probeConfig/{id}", method = RequestMethod.PUT)
-    public ProbeConfig save(final String input, @PathVariable("id") final long id)
-    {
-        LOG.info("Got Here: " + id + " \n\n\n\n\n\n\n\n\n" + input);
-        final ProbeConfig config = repo.findById(id).get();
-        LOG.info("Config: " + config);
-        return config;
-    }
-
-    @Secured("ROLE_ADMIN")
-    @RequestMapping(path = "/repo/probeConfigs", method = RequestMethod.GET)
-    public Iterable<ProbeConfig> getAll()
-    {
-        final Iterable<ProbeConfig> config = repo.findAll();
-        return config;
-    }
-
 }
