78f4b132db6f489066a88dc34dddf519f7525f9b
BaJaEm DRC <bajaemdrc@gmail.com>
2018-10-23 14:14:10 -0400

    Modify Basic Security Config to user the Users from the ProbeUsers

--- src/main/java/org/bajaem/drcmon/security/BasicSecurityConfig.java
+++ src/main/java/org/bajaem/drcmon/security/BasicSecurityConfig.java
@@ -3,6 +3,11 @@ package org.bajaem.drcmon.security;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bajaem.drcmon.configuration.SystemUserWrapper;
+import org.bajaem.drcmon.configuration.TypedWrapped;
+import org.bajaem.drcmon.configuration.Wrapped;
+import org.bajaem.drcmon.model.ProbeUser;
+import org.bajaem.drcmon.respository.ProbeUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +23,9 @@ public class BasicSecurityConfig extends DRCSecurityConfig
 
     private static final Logger LOG = LogManager.getLogger();
 
+    @Autowired
+    private ProbeUserRepository userRepo;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception
     {
@@ -30,7 +38,19 @@ public class BasicSecurityConfig extends DRCSecurityConfig
     @Autowired
     public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.inMemoryAuthentication().withUser("a").password("{noop}a").roles("USER").and().withUser("b")
-                .password("{noop}b").roles("ADMIN", "USER");
+        final TypedWrapped<Iterable<ProbeUser>> w = () -> userRepo.findAll();
+        final Iterable<ProbeUser> users = SystemUserWrapper.executeAsSystem(w);
+        for (final ProbeUser s : users)
+        {
+            final String user = s.getUserId();
+            if (s.isAdmin())
+            {
+                auth.inMemoryAuthentication().withUser(user).password("{noop}" + s.getSecret()).roles("ADMIN", "USER");
+            }
+            else
+            {
+                auth.inMemoryAuthentication().withUser(user).password("{noop}" + s.getSecret()).roles("USER");
+            }
+        }
     }
 }
